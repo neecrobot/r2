61eb21b4f0b9a55219f0cb1e943c895002058050
neecrobot <131327722+neecrobot@users.noreply.github.com>
2023-05-21 07:57:53 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,60 @@
+let POS = 0
+let X = 0
+let Y = 0
+let RSTOP = 1
+let RSTART = 0
+let DELAY = 100
+let STATE = 0
+basic.forever(function () {
+    if (STATE == 0 && input.buttonIsPressed(Button.B)) {
+        DELAY = 100
+        STATE = 1
+        RSTART = 1
+        while (RSTOP == 1) {
+            basic.pause(100)
+        }
+    } else if (STATE == 1 && !(input.buttonIsPressed(Button.B))) {
+        for (let index = 0; index < 5; index++) {
+            DELAY += 100
+            basic.pause(1000)
+        }
+        RSTART = 0
+        STATE = 2
+    } else if (STATE == 2 && RSTOP == 1) {
+        for (let index = 0; index < 4; index++) {
+            basic.showNumber(POS)
+            basic.pause(100)
+            basic.clearScreen()
+            basic.pause(100)
+        }
+        STATE = 0
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    for (let COUNT = 0; COUNT <= 15; COUNT++) {
+        while (0 == RSTART) {
+            RSTOP = 1
+            basic.pause(10)
+        }
+        RSTOP = 0
+        led.unplot(X, Y)
+        if (COUNT < 4) {
+            X = COUNT
+            Y = 0
+        } else if (COUNT < 8) {
+            X = 4
+            Y = COUNT - 4
+        } else if (COUNT < 12) {
+            X = 12 - COUNT
+            Y = 4
+        } else {
+            X = 0
+            Y = 16 - COUNT
+        }
+        POS = COUNT
+        led.plot(X, Y)
+        basic.pause(DELAY)
+    }
+})
